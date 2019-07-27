6d68a1bb1fbde0599c365feb92eae2699210690b
Simon Berger <simon@siku2.io>
2019-07-28 00:12:53 +0200

    wampli mirror library version

--- libwampli/__init__.py
+++ libwampli/__init__.py
@@ -3,4 +3,5 @@ from .connection import *
 from .format import *
 from .session import *
 
+__author__ = "Giesela Inc."
 __version__ = "0.1.1"
--- wampli/__init__.py
+++ wampli/__init__.py
@@ -1,7 +1,7 @@
 """WAMPli is a command line interface for the WAMP protocol."""
 
+import libwampli
 from .shell import *
 
-__author__ = "Giesela Inc."
-# TODO use libwampli version
-__version__ = "0.0.2"
+__author__ = libwampli.__author__
+__version__ = libwampli.__version__
